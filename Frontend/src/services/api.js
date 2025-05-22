d75df1ec97cd973c261eb59da88bbab8562dde99
Sakshi Mathur <147843432+sakshimathur03@users.noreply.github.com>
2025-05-22 11:47:28 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || "https://edusync-backend-api.azurewebsites.net/api";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "https://edusync-backend-api1.azurewebsites.net/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
